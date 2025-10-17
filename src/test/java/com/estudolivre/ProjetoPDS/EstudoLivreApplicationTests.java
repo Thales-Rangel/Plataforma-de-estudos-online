7542b94772275c7e9d1e53509a75a4fdb937e4a9
Thales-Rangel <thalesran2006@gmail.com>
2025-10-17 11:18:55 -0300

    Initial commit

---
+++ src/test/java/com/estudolivre/ProjetoPDS/EstudoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudolivre.ProjetoPDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
