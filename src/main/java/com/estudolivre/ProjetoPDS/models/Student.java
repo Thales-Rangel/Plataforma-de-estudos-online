745bf3daa775ed7c3c2a75483352071217eae6ae
Fernanda da Silva Pereira <20221204010005@ifrn.local>
2025-11-07 11:23:57 -0300

    Alterando projeto

--- src/main/java/com/estudolivre/ProjetoPDS/models/Student.java
+++ src/main/java/com/estudolivre/ProjetoPDS/models/Student.java
@@ -1,5 +1,28 @@
 package com.estudolivre.ProjetoPDS.models;
 
 public class Student extends Users {
+	
+	private String matricula;
+	private String curso;
+
+	public Student(int id, String nomeCompleto, String email, String senha, String matricula, String curso) {
+		super(id, nomeCompleto, email, senha);
+		this.curso = curso;
+		this.matricula = matricula;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
 
 }
--- src/main/java/com/estudolivre/ProjetoPDS/models/Teachers.java
+++ src/main/java/com/estudolivre/ProjetoPDS/models/Teachers.java
@@ -1,5 +1,41 @@
 package com.estudolivre.ProjetoPDS.models;
 
 public class Teachers extends Users {
+	
+	private String disciplina;
+	private String formacao; 
+	
+	public Teachers(int id, String nomeCompleto, String email, String senha, String disciplina, String formacao) {
+		super(id, nomeCompleto, email, senha);
+		this.disciplina = disciplina;
+		this.formacao = formacao;
+	}
+
+	private Users Users;
+
+	public String getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public String getFormacao() {
+		return formacao;
+	}
+
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+
+	public Users getUsers() {
+		return Users;
+	}
+
+	public void setUsers(Users users) {
+		Users = users;
+	}
+	
 
 }
