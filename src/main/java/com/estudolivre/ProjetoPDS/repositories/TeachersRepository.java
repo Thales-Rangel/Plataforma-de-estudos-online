4e2b54266dc463ae48cf51e9a4ef90f5c425a617
Thales-Rangel <thalesran2006@gmail.com>
2025-11-26 21:24:32 -0300

    Renomeando objetos professores

--- src/main/java/com/estudolivre/ProjetoPDS/repositories/TeachersRepository.java
+++ src/main/java/com/estudolivre/ProjetoPDS/repositories/TeachersRepository.java
@@ -3,9 +3,9 @@ package com.estudolivre.ProjetoPDS.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.estudolivre.ProjetoPDS.models.Teachers;
+import com.estudolivre.ProjetoPDS.models.Teacher;
 
 @Repository
-public interface TeachersRepository extends JpaRepository<Teachers, Long> {
+public interface TeachersRepository extends JpaRepository<Teacher, Long> {
 
 }
